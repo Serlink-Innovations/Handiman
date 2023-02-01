b4ed86f1f2c3d299fc89de3877a53430493fa62f
Serlink-Innovations <serlink.business@outlook.com>
2023-01-31 21:09:01 -0400

    v1.0

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer class="footer">
+
+    <div class="footer--container container mx-auto">
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ullam debitis quasi officiis, minima obcaecati
+            repudiandae excepturi et, asperiores atque animi ea iusto? Id ipsum dignissimos iste perspiciatis
+            accusantium autem?
+        </p>
+
+    </div>
+
+    <div class="copyright">
+
+        <label for="">Copyright Handiman 2023 | All rights reserved</label>
+
+    </div>
+
+</footer>
+
+</div>
+
+</body>
+
+</html>
---
+++ inc/navigation.php
@@ -0,0 +1,55 @@
+<nav class="navigation container mx-auto" role="navigation">
+
+    <a class="logo" href="">
+        <strong><span class="clr-primary">Handi</span><span>man</span></strong>
+    </a>
+
+    <ul class="nav">
+
+        <li class="nav-item">
+
+            <a href="" class="nav-link">Find Architect</a>
+
+        </li>
+
+        <li class="nav-item">
+
+            <a href="" class="nav-link">Find Civil Engineer</a>
+
+        </li>
+
+        <li class="nav-item">
+
+            <a href="" class="nav-link">Find Electrician</a>
+
+        </li>
+
+        <li class="nav-item">
+
+            <a href="" class="nav-link">More Freelancers</a>
+
+        </li>
+
+    </ul>
+
+    <ul class="nav--buttons">
+
+        <li>
+
+            <a href="">
+                Log In
+            </a>
+
+        </li>
+
+        <li>
+
+            <a class="btn btn-primary" href="">
+                Sign Up
+            </a>
+
+        </li>
+
+    </ul>
+
+</nav>
